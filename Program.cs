af67798c40394e13a78155ef19694edb5c7b14d7
Dasha06 <dasha.nikitina20@yandex.ru>
2024-12-04 17:57:41 +0300

    innit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿class MoveZeros
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите числа, разделенные запятыми:");
+        string input = Console.ReadLine();
+        int[] nums = Array.ConvertAll(input.Split(','), int.Parse);
+        MoveZeroes(nums);
+        Console.WriteLine(string.Join(", ", nums));
+    }
+
+    public static void MoveZeroes(int[] nums)
+    {
+        int lastNonZeroIndex = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[lastNonZeroIndex] = nums[i];
+                lastNonZeroIndex++;
+            }
+        }
+
+        for (int i = lastNonZeroIndex; i < nums.Length; i++)
+        {
+            nums[i] = 0;
+        }
+    }
+}
